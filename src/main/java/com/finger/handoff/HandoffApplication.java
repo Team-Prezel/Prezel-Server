66d7860e23dad06ecd640ebedfa04180a06a23fe
audwns03 <myeongjunkang@gmail.com>
2026-01-21 20:53:11 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/main/java/com/finger/handoff/HandoffApplication.java
@@ -0,0 +1,13 @@
+package com.finger.handoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandoffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/finger/handoff/HandoffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finger.handoff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandoffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
