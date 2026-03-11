72a5a6ac46358a373dfef16afcc42c55078683a5
Hyeonjun <kjun2003@naver.com>
2026-03-11 21:31:07 +0900

    feat : 유저 Entity 구현

---
+++ src/main/java/com/finger/handoff/domain/user/entity/User.java
@@ -0,0 +1,27 @@
+package com.finger.handoff.domain.user.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+
+    private String profileImgUrl;
+
+    private String refreshToken;
+}
