720717e5bc8e1d34d5230986df38b199ada74dc7
Hyeonjun <kjun2003@naver.com>
2026-03-22 22:16:01 +0900

    feat : 회원 정보 조회 구현 및 카카오 리다이렉트 주소 수정

---
+++ src/main/java/com/finger/handoff/domain/user/dto/UserDto.java
@@ -0,0 +1,16 @@
+package com.finger.handoff.domain.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class UserDto {
+    private Long id;
+
+    private String email;
+
+    private String profileImgUrl;
+}
--- src/main/java/com/finger/handoff/global/security/handler/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/finger/handoff/global/security/handler/CustomAuthenticationEntryPoint.java
@@ -15,11 +15,9 @@ public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
     private final HandlerExceptionResolver resolver;
 
-    // 스프링이 기본으로 제공하는 에러 토스용 객체(HandlerExceptionResolver)를 주입받습니다.
     public CustomAuthenticationEntryPoint(@Qualifier("handlerExceptionResolver") HandlerExceptionResolver resolver) {
         this.resolver = resolver;
     }
-
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) {
         resolver.resolveException(request, response, null, new BusinessException(ErrorCode.UNAUTHORIZED));
