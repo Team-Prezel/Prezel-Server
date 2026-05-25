3b6455e7ab2f5743fc23d6100af017799cb7cbde
Hyeonjun <kjun2003@naver.com>
2026-05-25 21:57:50 +0900

    feat : 뱃지 조회 구현 및 sse 스웨거 추가

--- src/main/java/com/finger/handoff/domain/badge/entity/UserBadge.java
+++ src/main/java/com/finger/handoff/domain/badge/entity/UserBadge.java
@@ -5,6 +5,9 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
@@ -20,10 +23,13 @@ public class UserBadge {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    // 🌟 뱃지 테이블 대신 Enum을 직접 저장! (EnumType.STRING 필수)
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 30)
     private BadgeType badgeType;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
 
     public UserBadge(User user, BadgeType badgeType) {
         this.user = user;
